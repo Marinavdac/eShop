b87eac688b6837f9da43d641099ef53f0a955341
Marinavdac <marinavdac@gmail.com>
2022-07-08 10:49:39 -0300

    feat: add input field

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 import PersistentDrawerLeft from '../components/PersistentDrawerLeft';
 import getCategoriesList from '../components/getCategoriesList';
+import { TextField } from '@mui/material';
+import './styles/Home.css';
 
 function Home() {
-  
   const categories = getCategoriesList();
 
-  console.log(categories)
+  console.log(categories);
   return (
     <div>
-      <h1>Home</h1>
+      <Stack direction="column" size="large" spacing={10} />
+      <PersistentDrawerLeft categories={categories} />
+      <section className="search-bar-section">
+      <TextField id="outlined-basic" label="Search" variant="outlined" />
+      <Button variant="contained">Buscar</Button>
+      </section>  
+       
       
-      <PersistentDrawerLeft categories={categories}/>
+
     </div>
-  )
-  
+  );
 }
 
 export default Home;
