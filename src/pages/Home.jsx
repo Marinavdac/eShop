7b31a54869946fd002079622fab035207943c324
Marinavdac <marinavdac@gmail.com>
2022-10-09 11:47:51 -0300

    build: removes @mui dependency and its elements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,5 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
-import PersistentDrawerLeft from '../components/PersistentDrawerLeft';
 import getCategoriesList from '../components/getCategoriesList';
-import { TextField } from '@mui/material';
 import './styles/Home.css';
 
 function Home() {
@@ -12,13 +8,7 @@ function Home() {
   console.log(categories);
   return (
     <div>
-      <Stack direction="column" size="large" spacing={10} />
-      <PersistentDrawerLeft categories={categories} />
-      <section className="search-bar-section">
-      <TextField id="outlined-basic" label="Search" variant="outlined" />
-      <Button variant="contained">Buscar</Button>
-      </section>  
-       
+<h1> teste</h1>       
       
 
     </div>
